af3d2df4149170817163efd76b509cd8228b1195
ibrahim mushtaha <ibrahim.mushtaha2@gmail.com>
2021-06-27 21:30:03 +0300

    try to use setState in setting.dart

--- lib/view/setting.dart
+++ lib/view/setting.dart
@@ -9,14 +9,52 @@ class Setting extends StatefulWidget {
 }
 
 class _Setting extends State<Setting> {
+
+  bool isSwitched = false;
+  var textValue = 'Switch is OFF';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(SETTINGS),
       ),
-      body: Container(color: Colors.grey.shade200,),
+      body: Container(
+          color: Colors.grey.shade200,
+          child: Center(
+            child: Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(textValue),
+                Switch(value: isSwitched, onChanged: toggleSwitch),
+              ],
+            ),
+            ),
+          )
+      ),
     );
   }
+
+  void toggleSwitch(bool value) {
+
+    if(isSwitched == false)
+    {
+      setState(() {
+        isSwitched = true;
+        textValue = 'Switch Button is ON';
+      });
+      print('Switch Button is ON');
+    }
+    else
+    {
+      setState(() {
+        isSwitched = false;
+        textValue = 'Switch Button is OFF';
+      });
+      print('Switch Button is OFF');
+    }
+  }
+
 
 }
